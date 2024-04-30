033c3416f09f2b2f17ae38af47837ee5d94256eb
Krzysztof <krzysiek.gugala@gmail.com>
2024-04-30 22:03:01 +0200

    solutionv1

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,93 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  Grades = 'grades'
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function averageGrade(student: Student): number {
+  return student.grades.reduce((sum, grade) => (
+    sum + grade
+  ), 0);
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(students: Student[],
+  sortBy: SortType,
+  order: SortOrder): Student[] {
+  let result = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+      result = result.sort((a: Student, b: Student) => {
+        if (order === 'asc') {
+          return a.name.localeCompare(b.name);
+        }
+
+        return b.name.localeCompare(a.name);
+      });
+      break;
+
+    case SortType.Surname:
+      result = result.sort((a: Student, b: Student) => {
+        if (order === 'asc') {
+          return a.surname.localeCompare(b.surname);
+        }
+
+        return b.surname.localeCompare(a.surname);
+      });
+      break;
+
+    case SortType.Age:
+      result = result.sort((a: Student, b: Student) => {
+        if (order === 'asc') {
+          return a.age - b.age;
+        }
+
+        return b.age - a.age;
+      });
+      break;
+
+    case SortType.Married:
+      result = result.sort((a, b) => {
+        if (a.married && !b.married) {
+          return -1;
+        }
+
+        if (!a.married && b.married) {
+          return 1;
+        }
+
+        return 0;
+      });
+      break;
+
+    case SortType.Grades:
+      result = result.sort((a, b) => {
+        if (order === 'asc') {
+          return averageGrade(a) - averageGrade(b);
+        }
+
+        return averageGrade(b) - averageGrade(a);
+      });
+
+      break;
+
+    default:
+      return result;
+  }
+
+  return result;
 }
